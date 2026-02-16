5ecead75dc71a905d3044b79cb5ae36ff083d1a9
Antoine FEROT <antoine.ferot@gmail.com>
2026-02-16 23:29:48 +0100

    auto-claude: subtask-1-2 - Create Sources/CMQC module map and shim header for IBM MQ C library interop

--- Sources/CMQC/shim.h
+++ Sources/CMQC/shim.h
@@ -1,10 +1,12 @@
-// CMQC Shim Header - Stub for Package.swift validation
-// Full implementation in subtask-1-2
+// CMQC Shim Header
+// Provides Swift interop with IBM MQ C Client library headers
+// Requires IBM MQ Client installed at /opt/mqm
+// Install: brew tap ibm-messaging/ibmmq && brew install --cask ibm-messaging/ibmmq/ibmmq
+
 #ifndef CMQC_SHIM_H
 #define CMQC_SHIM_H
 
-// Uncomment when IBM MQ Client is installed at /opt/mqm
-// #include "/opt/mqm/inc/cmqc.h"
-// #include "/opt/mqm/inc/cmqxc.h"
+#include "/opt/mqm/inc/cmqc.h"
+#include "/opt/mqm/inc/cmqxc.h"
 
 #endif
